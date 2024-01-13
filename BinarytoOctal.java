963bfb6143448f62eea4b0c13f8093e5c35879f4
dotseemajadhav1794 <155809173+dotseemajadhav1794@users.noreply.github.com>
2024-01-13 14:43:38 +0530

    Add files via upload

---
+++ BinarytoOctal.java
@@ -0,0 +1,42 @@
+//Java Program to Convert Binary Number to Octal and vice-versa
+public class BinarytoOctal 
+{
+    public static void main(String[] args) 
+		{
+			long binary = 101001;
+			int octal = convertBinarytoOctal(binary);
+			System.out.println(binary + " in binary = " + octal + " in octal");
+		}
+
+	public static int convertBinarytoOctal(long binaryNumber) 
+		{
+			int octalNumber = 0, decimalNumber = 0, i = 0;
+
+			while (binaryNumber != 0) 
+			{
+				decimalNumber += (binaryNumber % 10) * Math.pow(2, i);
+				++i;
+				binaryNumber /= 10;
+			}
+
+			i = 1;
+
+			while (decimalNumber != 0) 
+			{
+				octalNumber += (decimalNumber % 8) * i;
+				decimalNumber /= 8;
+				i *= 10;
+			}
+
+			return octalNumber;
+		}
+}   /*
+Binary To Decimal
+1 * 25 + 0 * 24 + 1 * 23 + 0 * 22 + 0 * 21 + 1 * 20 = 41
+
+Decimal to Octal
+8 | 41
+8 | 5 -- 1
+8 | 0 -- 5
+(51)
+*/
