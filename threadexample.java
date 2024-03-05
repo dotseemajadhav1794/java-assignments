23deb2b65865710917c1b6d590fe95935e4a8ef5
DESKTOP-77C0AMU\Admin <komal.dongare@ojassoft.in>
2024-03-05 16:53:58 +0530

    practice

---
+++ threadexample.java
@@ -0,0 +1,16 @@
+class table1 extents Thread
+{
+    run(){
+        for(int i=0;i<100;i++)
+        {
+            System.out.println(i);
+        }
+    start(){
+        System.out.println("we are in start.")
+    }
+    }
+    public static void main[String() args]{
+        table1 t= new table();
+        t.start();
+    }
+}
